2086654065bcb6ff70e26038549336d3c97eda05
Marvin <awuor.marvin5@gmail.com>
2015-10-21 17:32:38 +0300

    use jquery easy ui to implement drag and drop functions

--- script.js
+++ script.js
@@ -1,48 +1,54 @@
-
-$('.item').draggable({
-    revert:true,
-    proxy:'clone',
-    onStartDrag:function(){
-        $(this).draggable('options').cursor = 'not-allowed';
-        $(this).draggable('proxy').css('z-index',10);
-    },
-    onStopDrag:function(){
-        $(this).draggable('options').cursor='move';
-    }
-});
-$('.cart').droppable({
-    onDragEnter:function(e,source){
-        $(source).draggable('options').cursor='auto';
-    },
-    onDragLeave:function(e,source){
-        $(source).draggable('options').cursor='not-allowed';
-    },
-    onDrop:function(e,source){
-        var name = $(source).find('p:eq(0)').html();
-        var price = $(source).find('p:eq(1)').html();
-        addProduct(name, parseFloat(price.split('$')[1]));
-    }
-});
 var data = {"total":0,"rows":[]};
-var totalCost = 0;
-function addProduct(name,price){
-    function add(){
-        for(var i = 0; i<data.total; i++){
-            var row = data.rows[i];
-            if (row.name == name){
-                row.quantity += 1;
-                return;
-            }
-        }
-        data.total += 1;
-        data.rows.push({
-            name:name,
-            quantity:1,
-            price:price
+        var totalCost = 0;
+        
+        $(function(){
+            $('#cartcontent').datagrid({
+                singleSelect:true
+            });
+            $('.item').draggable({
+                revert:true,
+                proxy:'clone',
+                onStartDrag:function(){
+                    $(this).draggable('options').cursor = 'not-allowed';
+                    $(this).draggable('proxy').css('z-index',10);
+                },
+                onStopDrag:function(){
+                    $(this).draggable('options').cursor='move';
+                }
+            });
+            $('.cart').droppable({
+                onDragEnter:function(e,source){
+                    $(source).draggable('options').cursor='auto';
+                },
+                onDragLeave:function(e,source){
+                    $(source).draggable('options').cursor='not-allowed';
+                },
+                onDrop:function(e,source){
+                    var name = $(source).find('p:eq(0)').html();
+                    var price = $(source).find('p:eq(1)').html();
+                    addProduct(name, parseFloat(price.split('.')[1]));
+                }
+            });
         });
-    }
-    add();
-    totalCost += price;
-    $('#cartcontent').datagrid('loadData', data);
-    $('div.cart .total').html('Total: Kshs.'+totalCost);
-}
+        
+        function addProduct(name,price){
+            function add(){
+                for(var i=0; i<data.total; i++){
+                    var row = data.rows[i];
+                    if (row.name == name){
+                        row.quantity += 1;
+                        return;
+                    }
+                }
+                data.total += 1;
+                data.rows.push({
+                    name:name,
+                    quantity:1,
+                    price:price
+                });
+            }
+            add();
+            totalCost += price;
+            $('#cartcontent').datagrid('loadData', data);
+            $('div.cart .total').html('Total: Kshs: '+totalCost);
+        }
